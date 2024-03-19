0b76d2635d712265504e913d7dee7585c6ef78a1
Vitor Hugo A <57122307+toruggo@users.noreply.github.com>
2024-03-19 18:27:32 -0300

    Cpp scarecrow solution

--- Greedy/C++/espantalho.cpp
+++ Greedy/C++/espantalho.cpp
@@ -1,30 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
-    int n;
-    string s;
-
-    cin >> n >> s;
-
-    int ans = 0;
-    for(int i = 0; i < n; i++){
-        if(s[i] == '.'){
-            i += 2;
-            ans++;
+class Solution {
+public:
+    int minimumScarecrows(int n, string field) {
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(field[i] == '.'){
+                i += 2;
+                ans++;
+            }
         }
+        return ans;
     }
-
-    cout << ans << endl;
-}
+};
 
 int main(){
     int t;
     cin >> t;
 
+    int n;
+    string s;
+    Solution solve;
     for(int i = 1; i <= t; i++){
-        cout << "Case " << i << ": ";
-        solve();
+        cin >> n >> s;
+        cout << "Case " << i << ": " << solve.minimumScarecrows(n, s) << endl;
     }
 }
